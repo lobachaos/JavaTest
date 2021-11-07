990f4f3eec514a52ad14d60112ae02c6ed04dc58
lobachaos <zwolfsasa@gmail.com>
2021-11-06 22:36:53 -0400

    Pilha

---
+++ JavaTestes/src/estruturaDeDados/Main.java
@@ -0,0 +1,10 @@
+package estruturaDeDados;
+
+public class Main {
+    public static void main(String[] args) {
+        No<String> noUm = new No<>("Conteudo01");
+        System.out.println(noUm);
+        No<Integer> noDois = new No<>(123);
+        System.out.println(noDois);
+    }
+}
---
+++ JavaTestes/src/estruturaDeDados/Pilha.java
@@ -0,0 +1,39 @@
+package estruturaDeDados;
+
+public class Pilha {
+    private No refNo;
+
+    public Pilha() {
+        this.refNo = null;
+    }
+
+    public No getRefNo() {
+        return refNo;
+    }
+
+    public void setRefNo(No refNo) {
+        this.refNo = refNo;
+    }
+
+    public boolean isEmpty() {
+        return refNo == null;
+    }
+
+    public No top(No no){
+        return refNo;
+    }
+
+    public void push(No NovoNo){
+        No noAux = refNo;
+        refNo = NovoNo;
+        refNo.setProximoNo(noAux);
+    }
+
+    public void pop(){
+        if(!isEmpty())
+        refNo = refNo.getProximoNo();
+    }
+
+
+
+}
